17113d55d3bec76e4363fb7a1ca721df13b17e9a
Matheus Minetto <matheusminetto140502@gmail.com>
2026-04-24 19:12:34 -0300

    feat: 'habilitação do cors para chamadas api'

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import cors from "cors";
 
 const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // const users = [];
 
@@ -41,7 +43,7 @@ app.get("/users", async (req, res) => {
 app.put("/users/:id", async (req, res) => {
   console.log(req);
   await prisma.user.update({
-    where: {
+    where: {  
       id: req.params.id,
     },
     data: {
