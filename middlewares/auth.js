aa98587baef43175d23b478ab98fd2bbb560db89
Matheus Minetto <matheusminetto140502@gmail.com>
2026-04-30 00:59:33 -0300

    issue-3 feat: 'adição do userId para saber quem é o usuário autenticação'

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,6 +9,7 @@ const auth = (req, res, next) => {
   }
   try {
     const decoded = jwt.verify(token.replace("Bearer ", ""), JWT_SECRET);
+    req.userId = decoded.id;
   } catch (err) {
     return res.status(401).json({ message: "Token inválido" });
   }
